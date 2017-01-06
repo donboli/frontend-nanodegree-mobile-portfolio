1a04adf35f267dbc6e202448c257e3b48739b223
Juan Becker <wolli.becker@gmail.com>
2017-01-06 21:16:39 +0100

    Perform pagespeed insights CSS and JS optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,25 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
+gulp.task('default', ['clean'], function() {
   var inline = require('gulp-inline')
     , uglify = require('gulp-uglify')
     , minifyCss = require('gulp-minify-css')
-    , autoprefixer = require('gulp-autoprefixer')
     , rename = require('gulp-rename');
 
   gulp.src('src_index.html')
     .pipe(inline({
-      base: 'public/',
+      base: './',
       js: uglify,
-      css: [minifyCss, autoprefixer({ browsers:['last 2 versions'] })],
-      disabledTypes: ['svg', 'img', 'js'], // Only inline css files
-      ignore: ['./css/print.css']
+      css: [minifyCss],
+      disabledTypes: ['svg', 'img'], // Only inline css files
+      ignore: ['css/print.css']
     }))
     .pipe(rename('index.html'))
     .pipe(gulp.dest('./'));
 });
 
-gulp.task('remove_index', function () {
-  gulp.src('./index.html', {read: false})
-    .pipe(clean());
-});
+gulp.task('clean', function () {
+  var del = require('del');
 
-gulp.task('process', ['remove_index', 'default']);
+  return del('./index.html');
+});
